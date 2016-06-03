e7f4b044a64974fb5d65b36381fcde8fac0ec2e4
YtGz <Philipp.Wirtenberger@student.uibk.ac.at>
2016-06-03 09:48:09 +0200

    clean up library code

--- lib/term-rewriting/src/Data/Rewriting/Substitution.hs
+++ lib/term-rewriting/src/Data/Rewriting/Substitution.hs
@@ -4,24 +4,13 @@
 -- Authors: Bertram Felgenhauer, Martin Avanzini
 
 module Data.Rewriting.Substitution (
-    GSubst,
-    Subst,
-    -- * Important operations
-    gApply,
-    apply,
-    compose,
     -- * Reexported modules
-    module Data.Rewriting.Substitution.Type,
-    module Data.Rewriting.Substitution.Ops,
-    module Data.Rewriting.Substitution.Match,
-    module Data.Rewriting.Substitution.Unify,
-    module Data.Rewriting.Substitution.Pretty,
-    module Data.Rewriting.Substitution.Parse,
+    module X
 ) where
 
-import Data.Rewriting.Substitution.Type hiding (fromMap, toMap)
-import Data.Rewriting.Substitution.Ops
-import Data.Rewriting.Substitution.Match
-import Data.Rewriting.Substitution.Unify
-import Data.Rewriting.Substitution.Pretty
-import Data.Rewriting.Substitution.Parse
+import Data.Rewriting.Substitution.Type as X hiding (fromMap, toMap)
+import Data.Rewriting.Substitution.Ops as X
+import Data.Rewriting.Substitution.Match as X
+import Data.Rewriting.Substitution.Unify as X
+import Data.Rewriting.Substitution.Pretty as X
+import Data.Rewriting.Substitution.Parse as X
--- lib/term-rewriting/src/Data/Rewriting/Term.hs
+++ lib/term-rewriting/src/Data/Rewriting/Term.hs
@@ -5,17 +5,12 @@
 
 module Data.Rewriting.Term (
     Term (..),
-    -- * Important operations
-    fold, map, vars, funs,
     -- * Reexported modules
-    module Data.Rewriting.Term.Type,
-    module Data.Rewriting.Term.Ops,
-    module Data.Rewriting.Term.Pretty,
-    module Data.Rewriting.Term.Parse
+    module X
 ) where
 
 import Prelude ()
-import Data.Rewriting.Term.Type
-import Data.Rewriting.Term.Ops
-import Data.Rewriting.Term.Pretty
-import Data.Rewriting.Term.Parse
+import Data.Rewriting.Term.Type as X
+import Data.Rewriting.Term.Ops as X
+import Data.Rewriting.Term.Pretty as X
+import Data.Rewriting.Term.Parse as X
