703cdc146c97c05ea4ca41c5e68a9d82eb88912f
YtGz <Philipp.Wirtenberger@student.uibk.ac.at>
2016-07-09 18:38:20 +0200

    finalize EVAL rule

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,7 @@ import qualified Data.Rewriting.Substitution.Type as Subst
 import           Data.Rewriting.Term.Type                (Term (..))
 import           SymbolicEvaluationGraphs.InferenceRules
 import           Data.Implicit
+import           Data.Map
 
 --main = print (suc ([(Hole, "")],([AVar "T1", AVar "T2"],[])))
 
@@ -29,21 +30,13 @@ import           Data.Implicit
   (exprs,_) <- parseProlog2 (head as)
   print (map exprToClause exprs)-}
 
-{-main = do
-  as <- getArgs
-  (exprs,_) <- parseProlog2 (head as)
-  let clauses = map termToClause (map exprToTerm (filter (not . isQuery) exprs))
-  print (caseRule clauses ([(Term (Fun "add" [Fun "0" [], Fun "s" [Fun "0" []]]), Subst.fromMap (Data.Map.fromList [("X", Data.Rewriting.Term.Type.Var "X")]) ,Nothing)],([],[])))-}
+-- main = print (caseRule test ([(Term (Fun "add" [Fun "Left 0" [], Fun "s" [Fun "Left 0" []]]), Subst.fromMap (Data.Map.fromList [("X", Data.Rewriting.Term.Type.Var "X")]), Nothing)],([],[])))
 
---main = print (unify' (Fun "star" [Var "T1", Var "T2"]) (Fun "star" [Var "XS", Fun "[]" []]))
+-- main = print (unify' (Fun "star" [Var "T1", Var "T2"]) (Fun "star" [Var "XS", Fun "[]" []]))
 
- {-main = do
-  as <- getArgs
-  (exprs,_) <- parseProlog2 (head as)
-  let clauses = map termToClause (map exprToTerm (filter (not . isQuery) exprs))
-  print (eval clauses ([(Term (Fun "add" [Fun "0" [], Fun "s" [Fun "0" []]]), Subst.fromMap (Data.Map.fromList [("X", Data.Rewriting.Term.Type.Var "X")]) ,Nothing)],([],[])))-}
+main = print (eval test (caseRule test ([(Term (Fun "add" [Fun "Left 0" [], Fun "s" [Fun "Left 0" []]]), Subst.fromMap (Data.Map.fromList [("X", Data.Rewriting.Term.Type.Var "X")]), Nothing)],([],[]))))
 
-main = print test
+-- main = print test
 
 test :: Implicit_ [Clause] => [Clause]
 test = param_
--- src/SymbolicEvaluationGraphs/InferenceRules.hs
+++ src/SymbolicEvaluationGraphs/InferenceRules.hs
@@ -1,18 +1,33 @@
 module SymbolicEvaluationGraphs.InferenceRules where
 
-import ExprToTerm.Conversion
-import Data.Rewriting.Term.Type(Term(..))
-import qualified Data.Rewriting.Term
-import Data.Rewriting.Substitution
 import Data.Maybe
+import qualified Data.Map (elems, filter, filterWithKey)
+import Control.Arrow
+import Data.Rewriting.Substitution
+import Data.Rewriting.Substitution.Type (fromMap, toMap)
+import qualified Data.Rewriting.Term
+import Data.Rewriting.Term.Type (Term(..))
+import ExprToTerm.Conversion
 
 type AbstractState = (State, KnowledgeBase)
+
 type KnowledgeBase = (G, U)
+
 type G = [AbstractVariable]
-data AbstractVariable = AVar String deriving(Show)
+
+data AbstractVariable =
+    AVar String
+    deriving (Eq,Show)
+
 type U = [(Term', Term')]
+
 type State = [(Goal, Subst', Maybe Clause)]
-data Goal = Term Term' | Hole deriving(Show)
+
+data Goal
+    = Term Term'
+    | Hole
+    deriving ((((Show))))
+
 type Clause = (Term', [Term']) -- h :- B
 
 suc :: AbstractState -> AbstractState
@@ -20,17 +35,56 @@ suc (state@((Hole,_,Nothing):_),kb) = (tail state, kb)
 suc _ = error "Cannot apply 'suc': Malformed AbstractState"
 
 caseRule :: [Clause] -> AbstractState -> AbstractState
-caseRule clauses ((Term t,sub,Nothing):s, kb) = ((let f clauseArray term substitution = if null clauseArray then [] else (Term term, substitution, head clauseArray) : f (tail clauseArray) term substitution in f) (map Just (slice clauses t)) t sub ++ s,kb)
+caseRule clauses ((Term t,sub,Nothing):s,kb) =
+    ( (let f clauseArray term substitution =
+               if null clauseArray
+                   then []
+                   else (Term term, substitution, head clauseArray) :
+                        f (tail clauseArray) term substitution
+       in f)
+          (map Just (slice clauses t))
+          t
+          sub ++
+      s
+    , kb)
+caseRule _ _ = error "Cannot apply 'caseRule': Malformed AbstractState"
 
 eval :: [Clause] -> AbstractState -> [AbstractState] --TODO: apply mgu also to KB
-eval clauses ((Term t,sub,Just (h,b)):s, (g,u)) = let (Just mgu) = unify' t h in [(map (\x -> (Term (apply mgu x),compose sub mgu,Nothing)) b ++ map (\(Term t',s',c') -> (Term (apply mgu t'),compose s' mgu,c')) s,(g,u)), (s, (g, u++[(t, h)]))]
+eval clauses ((Term t,sub,Just (h,b)):s,(g,u)) =
+    let (Just mgu) = unify' t h
+        mguG = restrictSubstToG mgu g
+    in [ ( map
+               (\x ->
+                     (Term (apply mgu x), compose sub mgu, Nothing))
+               b ++
+           map
+               (\(Term t',s',c') ->
+                     (Term (apply mguG t'), compose s' mguG, c'))
+               s  {-use new abstractVars-}
+         , ( map
+                 (\(Var v) ->
+                       AVar v)
+                 (Data.Map.elems
+                      (Data.Map.filter
+                           (\x ->
+                                 case x of
+                                     Var _ -> True
+                                     _ -> False)
+                           (toMap mguG)))
+           , map (apply mguG *** apply mguG) u))
+       , (s, (g, u ++ [(t, h)]))]
+eval _ _ = error "Cannot apply 'eval': Malformed AbstractState"
 
 root :: Term' -> String
 root (Var s) = s
 root (Fun s _) = s
 
 slice :: [Clause] -> Term' -> [Clause]
-slice clauses t = filter (\x -> root (fst x) == root t) clauses
+slice clauses t =
+    filter
+        (\x ->
+              root (fst x) == root t)
+        clauses
 
 termToClause :: Term' -> Clause
 termToClause (Fun ":-" args) = (head args, tail args) -- rule
@@ -38,10 +92,23 @@ termToClause h@(Fun _ _) = (h, []) -- fact (empty body)
 termToClause _ = error "Cannot apply 'exprToClause': Malformed Clause"
 
 -- unify, introducing fresh abstract variables
-unify' :: Term' -> Term' -> Maybe Subst'
-unify' t1 t2 = let vs = map Var (Data.Rewriting.Term.vars t1) in unify (Fun "freshVariables" (vs ++ [t2])) (Fun "freshVariables" (map freshVariable vs ++ [t1])) -- note the argument order: use unify h t instead of unify t h to ensure mapping from variables (element V) to abstract variables (element A)
+unify'
+    :: Term' -> Term' -> Maybe Subst'
+unify' t1 t2 =
+    let vs = map Var (Data.Rewriting.Term.vars t1)
+    in unify
+           (Fun "freshVariables" (vs ++ [t2]))
+           (Fun "freshVariables" (map freshVariable vs ++ [t1])) -- note the argument order: use unify h t instead of unify t h to ensure mapping from variables (element V) to abstract variables (element A)
 
 freshVariable :: Term' -> Term'
 freshVariable (Var v) = Var (v ++ "'") -- TODO: find alternative way of renaming that is more readable (e.g. with static counter)
+
+restrictSubstToG :: Subst' -> G -> Subst'
+restrictSubstToG sub g =
+    fromMap
+        (Data.Map.filterWithKey
+             (\k _ ->
+                   elem (AVar k) g)
+             (toMap sub))
 
 -- isSubstCompatibleToKB      G: AVars in G need to map to ground terms (or another AVar from G),   + U???
