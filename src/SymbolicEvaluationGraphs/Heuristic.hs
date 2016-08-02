ab19662294fd7c02a96a0f1bd20ac552cb199f18
YtGz <Philipp.Wirtenberger@student.uibk.ac.at>
2016-08-02 20:48:02 +0200

    add draft of heuristic
    this commit is only for backup purposes and not executable

--- src/SymbolicEvaluationGraphs/Heuristic.hs
+++ src/SymbolicEvaluationGraphs/Heuristic.hs
@@ -9,18 +9,40 @@ import Data.Either.Utils
 import ExprToTerm.Conversion
 import Data.Rewriting.Term.Type (Term(..))
 import SymbolicEvaluationGraphs.Types
+import SymbolicEvaluationGraphs.InferenceRules (suc, caseRule, eval, backtrack, isBacktrackingApplicable, split)
 import Diagrams.TwoD.Layout.Tree
 import Data.Rewriting.Term (vars, root)
 
+minExSteps :: Int
+minExSteps = 1
 maxBranchingFactor :: Int
 maxBranchingFactor = 4
+
+applyRule :: AbstractState -> Int -> BTree (AbstractState, String)
+applyRule s@([],_) n = leaf (s, "") -- just for output (base case of recursion)
+applyRule s@(([],_,_):_,_) n = BNode (s, "suc") (applyRule (suc s) n) Empty
+applyRule s@((_,_,Nothing):_,_) n =
+    BNode (s, "case") (applyRule (caseRule s) (n+1)) Empty
+applyRule s n =
+    if isBacktrackingApplicable s
+        then BNode (s, "backtrack") (applyRule (backtrack s) n) Empty
+        else if n >= minExSteps && (case s of ((t:_,_,Nothing):_,_) -> isFunctionSymbolRecursive (Fun (fromRight (root t)) [])
+                                              ((_:_,_,Just clause):_,_) -> isClauseRecursive clause) then fromMaybe (case s of (([_],_,_):_,_) -> e
+                                                                                                                               _ -> split s) i else e
+  where
+    ss = eval s
+    s1 = fst ss
+    s2 = snd ss
+    i = case s of ((t:_,_,Nothing):_,_) ->  BNode (s, "instance") (applyRule (tryToApplyInstanceRule s (getInstanceCandidates (BNode s Empty Empty) g{-TODO add current graph to args (-> use zippers?)-})) n) Empty --TODO: terminate here
+                  _ -> Nothing
+    e = BNode (s, "eval") (applyRule s1 n) (applyRule s2 n)
 
 getInstanceCandidates
     :: BTree (AbstractState, String)
     -> BTree (AbstractState, String)
     -> String
     -> [(AbstractState, String)]
-getInstanceCandidates (BNode node _ _) graph program =
+getInstanceCandidates (BNode node _ _) graph =
     filter
         (\x ->
               snd x /= "instance" &&
@@ -40,6 +62,9 @@ getVarNum (ss,_) =
              (\(qs,_,_) ->
                    concatMap vars qs)
              ss)
+
+isClauseRecursive :: Clause -> Bool
+isClauseRecursive (_,b) = any (\x -> isFunctionSymbolRecursive (Fun (fromRight (root x)) [])) (getMetaPredicates b)
 
 isFunctionSymbolRecursive
     :: Implicit_ [Clause]
@@ -187,3 +212,6 @@ branchingFactor
 branchingFactor (Fun f _) =
     length (filter (== Right f) (map (root . fst) (param_ :: [Clause])))
 branchingFactor _ = error "No function symbol provided."
+
+tryToApplyInstanceRule :: AbstractState -> [AbstractState] -> Maybe AbstractState
+tryToApplyInstanceRule s candidates = Nothing --TODO
